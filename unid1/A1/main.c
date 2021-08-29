83a2413de314490f7953468da6cf4cbcc03d5adb
camila monteiro <monteirocamis@gmail.com>
2021-08-29 00:06:29 -0300

    :construction:saiba mais and :memo: gitignore update

---
+++ unid1/A1/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/*
+Caro estudante,
+
+Em linguagens de programação estruturadas, tal como a linguagem C/C++ temos a possibilidade de organizar as informações a serem manipuladas sob dois aspectos: organização homogênea e organização heterogênea. No viés homogêneo, todas as informações, possuidoras de um único tipo de dados, estão dispostas em uma estrutura denominada vetor. E, por sua vez, no viés heterogêneo, as informações, podendo possuir tipagens distintas, estão armazenadas em estruturas de dados denominadas como “struct”.
+
+Com base nisso, suponha que é necessário armazenar informações relacionadas à veículos, totalizando, no máximo, 10 veículos. Para cada veículo, deverão ser fornecidos os seguintes dados:
+
+- marca do veículo
+
+- modelo - ano de fabricação
+
+- placa (formato XXX-YYYY; onde X denota as letras e Y denota os números da placa).
+
+O sistema deverá oferecer, ao usuário, as seguintes funcionalidades:
+
+- Listar os veículos cadastrados;
+
+- Inserir um novo veículo;
+
+- Listar os veículos filtrando-se por ano de fabricação;
+
+- Listar os veículos com o ano de fabricação acima de um certo valor especificado pelo usuário
+
+. - Listar os veículos filtrando-se pelo modelo.
+
+O sistema deverá armazenar os veículos ordenados pelo ano de fabricação, ou seja, ao inserir um novo veículo, este deve ser inserido em ordem crescente de ano de fabricação.
+
+Referência DEITEL, P. J.; DEITEL, H. C: Como Programar. 6. ed. São Paulo: Pearson Prentice Hall, 2011. [Recurso eletrônico, Biblioteca Virtual Universitári
+*/
+
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
---
+++ unid1/README.md
@@ -0,0 +1,22 @@
+# *tecnicasDeProgramacao-UAM UNID 1* 
+
+## arquivos da video aula, da anhembi morumbi .
+✔️ projeto 1 <br/>
+✔️ projeto 2 <br/>
+✔️ projeto 3 <br/>
+🚧 projeto 4<br/>
+ <br/>
+feitos em IDE codeblocks , no linux com ming
+ <br/> <br/>
+### 1. install mingw
+sudo apt install mingw-w64
+[win-builds.org/doku.php]
+
+### 2. install codeblocks
+ sudo apt install mingw-w64
+ [https://www.codeblocks.org/downloads/binaries/]
+
+### VScode & terminal 
+  cd projeto1 <br/>
+  g++ main.c -o meuprograma  <br/> 
+  ./meuprograma <br/> 
