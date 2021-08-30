eb0262b9f727529e2e63bb28bc6dc35025cfb375
camila monteiro <monteirocamis@gmail.com>
2021-08-30 05:30:25 -0300

    :art: add: for

--- unid1/meSalva1/main.c
+++ unid1/meSalva1/main.c
@@ -4,6 +4,8 @@
 
 int main()
 {
+
+     /* exemplo1 IF ELSE
     int val1, val2, soma;
     printf("digite o primeiro valor \n");
     scanf("%d",&val1);
@@ -11,8 +13,95 @@ int main()
       printf("digite o segundo valor \n");
         scanf("%d",&val2);
 
-        soma = val1 + val2;
+
+
+
+
+          if(val1 <0 || val2<0){
+            printf("\n erro: numero negativo");
+           }
+           else{
+            soma = val1 + val2;
            printf("resultado da soma: %d",soma);
+           }
+*/
+
+// exemplo2 WHILE
+
+//inicializar as variaveis
+/*
+int val1 = -1;
+int val2 = -1;
+int soma;
+
+
+//antes do uso
+while( val1 <=0 || val2 <= 0){
+
+    printf("digite o primeiro valor \n");
+    scanf("%d",&val1);
+
+      printf("digite o segundo valor \n");
+        scanf("%d",&val2);
+}
+ soma = val1 + val2;
+           printf("resultado da soma: %d",soma);
+
+*/
+
+           // exemplo3 DO WHILE
+           int val1 ;
+int val2;
+int soma;
+
+do{
+    printf("digite o primeiro valor \n");
+    scanf("%d",&val1);
+
+      printf("digite o segundo valor \n");
+        scanf("%d",&val2);
+}
+while( val1 <=0 || val2 <= 0);
+
+    soma = val1 + val2;
+           printf("resultado da soma: %d",soma);
+
 
         return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
